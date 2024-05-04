c5d95f616b62f35b345ec91e1cd8dd9ceb0c2437
x4mv <contactsscu@gmail.com>
2024-05-04 15:43:07 -0400

    arrays in c

---
+++ c-programming/llpc/arrays.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define MAX_ARR 10
+
+int main(){
+
+    // Definir un array de 10 elementos 
+    int arr[MAX_ARR]; 
+
+    // Definir el 4to elemento del array a 2 
+    
+    arr[3] = 2;
+
+    // imprimir el 4to elemento
+
+    printf("%d\n", arr[3]);
+
+
+    return 0;
+}
