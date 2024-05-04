e3f11a8d30e95a9f7ef3d2a1e24d1c1aa292ff04
x4mv <contactsscu@gmail.com>
2024-05-03 21:01:47 -0400

    llpcc 1

--- c-programming/main.c
+++ c-programming/main.c
@@ -1,11 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void greetings(char name[], int age);
+
+int cube(int number);
+
 int main() 
 {
-	char a[] = "tererePY";
-	int age = 21;
-	char p[] = "name";
-	printf("Hello my %s is: %s and i'm %dyo\n", p, a, age);
-	return 0;
+    double num1;
+    double num2;
+    int number; 
+    int result;
+
+    printf("Please enter the first number: ");
+    scanf("%lf", &num1);
+
+
+    printf("Please enter the second number: ");
+    scanf("%lf", &num2);
+    
+    printf("the numbers adds: %f \n", num1 + num2);
+
+    greetings("sergio", 21);
+    
+    printf("You gotta enter a number to apply a cube fn: \n");
+    scanf("%d", &number);
+
+    result = cube(number); 
+
+    if (result != 0 )
+    {
+        printf("The cube is: %d \n", result);
+    }else{
+        printf("This should be a even number >:/");
+    }
+    return 0;
+}
+
+void greetings(char name[], int age)
+{
+    printf("Hello %s, your age is: %d\n", name, age);
+}
+
+int cube(int number)
+{
+   if (number % 2 == 0 )
+   {
+        return 0;
+   }
+    return number * number * number;
 }
